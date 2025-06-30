d469537b39784dc63d9601d3af430b87f95860ad
Andrew Tokeley <andrewtokeley@gmail.com>
2025-07-01 11:31:06 +1200

    Changed Home Text

--- hikersteps/Views/HomeView.swift
+++ hikersteps/Views/HomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationStack() {
-            Text("HOME")
+            Text("LIST OF HIKES")
             NavigationLink {
                 HikeView()
             } label: {
