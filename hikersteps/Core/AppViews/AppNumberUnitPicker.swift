e2005a024accf854f88c4c1a77a1e3df2a6e409c
Andrew Tokeley <andrewtokeley@gmail.com>
2025-08-23 17:33:24 +1200

    checkin

---
+++ hikersteps/Core/AppViews/AppNumberUnitPicker.swift
@@ -0,0 +1,18 @@
+//
+//  AppNumberUnitPicker.swift
+//  hikersteps
+//
+//  Created by Andrew Tokeley on 22/08/2025.
+//
+
+import SwiftUI
+
+struct AppNumberUnitPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppNumberUnitPicker()
+}
---
+++ hikersteps/Features/Auth/AuthProvider.swift
@@ -0,0 +1,8 @@
+//
+//  AuthProvider.swift
+//  hikersteps
+//
+//  Created by Andrew Tokeley on 22/08/2025.
+//
+
+import Foundation
